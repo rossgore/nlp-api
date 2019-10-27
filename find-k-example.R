8450a3db17117a2de71b4198e51dabe25726bdee
rossgore <ross.gore@gmail.com>
2019-10-27 11:59:09 -0700

    Update find-kmeans-example.R
    Update find-kmeans-example.R

--- find-k-example.R
+++ find-k-example.R
@@ -6,6 +6,7 @@ library(stm)
 library(furrr)
 plan(multiprocess)
 library(dplyr)
+source("topic-model-api.R")
 
 normit <- function(x){((x-min(x))/(max(x)-min(x)))+.000001}
 
@@ -14,68 +15,39 @@ majority.vote <- function(x) {
   ux[which.max(tabulate(match(x, ux)))]
 }
 
-sherlock_raw <- gutenberg_download(1661)
 
-sherlock <- sherlock_raw %>%
-  mutate(story = ifelse(str_detect(text, "ADVENTURE"),
-                        text,
-                        NA)) %>%
-  fill(story) %>%
-  filter(story != "THE ADVENTURES OF SHERLOCK HOLMES") %>%
-  mutate(story = factor(story, levels = unique(story)))
+file.list = list.files("global_research/") 
+# try and create a doc term matrix from a list of pdf and html docs
+my.list.of.docs = paste0("global_research/", file.list)
 
-sherlock
+my.docs.df = from.a.list.of.files.to.file.text.df(my.list.of.docs)
 
-tidy_sherlock <- sherlock %>%
+tidy.docs.df <- my.docs.df %>%
   mutate(line = row_number()) %>%
   unnest_tokens(word, text) %>%
-  anti_join(stop_words) %>%
-  filter(word != "holmes")
+  anti_join(stop_words)
 
-tidy_sherlock %>% count(word, sort = TRUE)
+tidy.docs.dfm <- tidy.docs.df %>%
+  count(document, word, sort = TRUE) %>%
+  cast_dfm(document, word, n)
 
+tidy.docs.sparse <- tidy.docs.df %>%
+  count(document, word, sort = TRUE) %>%
+  cast_sparse(document, word, n)
 
-sherlock_tf_idf <- tidy_sherlock %>%
-  count(story, word, sort = TRUE) %>%
-  bind_tf_idf(word, story, n) %>%
-  arrange(-tf_idf) %>%
-  group_by(story) %>%
-  top_n(10) %>%
-  ungroup
-
-sherlock_tf_idf %>%
-  mutate(word = reorder_within(word, tf_idf, story)) %>%
-  ggplot(aes(word, tf_idf, fill = story)) +
-  geom_col(alpha = 0.8, show.legend = FALSE) +
-  facet_wrap(~ story, scales = "free", ncol = 3) +
-  scale_x_reordered() +
-  coord_flip() +
-  theme(strip.text=element_text(size=11)) +
-  labs(x = NULL, y = "tf-idf",
-       title = "Highest tf-idf words in Sherlock Holmes short stories",
-       subtitle = "Individual stories focus on different characters and narrative elements")
-
-sherlock_dfm <- tidy_sherlock %>%
-  count(story, word, sort = TRUE) %>%
-  cast_dfm(story, word, n)
-
-sherlock_sparse <- tidy_sherlock %>%
-  count(story, word, sort = TRUE) %>%
-  cast_sparse(story, word, n)
-
-topic_model <- stm(sherlock_dfm, K = 6, 
+topic_model <- stm(tidy.docs.dfm, K = 6, 
                    verbose = FALSE, init.type = "Spectral")
 
 many_models <- tibble(K = c(3, 4, 5, 6, 7, 8)) %>%
-  mutate(topic_model = future_map(K, ~stm(sherlock_sparse, K = .,
+  mutate(topic_model = future_map(K, ~stm(tidy.docs.sparse, K = .,
                                           verbose = FALSE)))
-heldout <- make.heldout(sherlock_sparse)
+heldout <- make.heldout(tidy.docs.sparse)
 
 k_result <- many_models %>%
   mutate(exclusivity = map(topic_model, exclusivity),
-         semantic_coherence = map(topic_model, semanticCoherence, sherlock_sparse),
+         semantic_coherence = map(topic_model, semanticCoherence, tidy.docs.sparse),
          eval_heldout = map(topic_model, eval.heldout, heldout$missing),
-         residual = map(topic_model, checkResiduals, sherlock_sparse),
+         residual = map(topic_model, checkResiduals, tidy.docs.sparse),
          bound =  map_dbl(topic_model, function(x) max(x$convergence$bound)),
          lfact = map_dbl(topic_model, function(x) lfactorial(x$settings$dim$K)),
          lbound = bound + lfact,
