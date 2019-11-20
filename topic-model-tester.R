27570dbd84294ff360ec48dfcf892427104ec64c
rossgore <ross.gore@gmail.com>
2019-11-20 10:08:44 -0500

    Notes from Meeting on 11/20/19
    See notes from Meeting on 11/20/19:
    (1) Be consistent with TRUE and T
    (2) no need to do dtm or lda JSON functions in tester
    (3) be sure to call with every parameter in tester
    (4) use validate after json function to show it returns valid json
    (5) for plots return path of saved pdf; update wordcloud functions in api to do this; but remove worldcloud functions with JSON in tester.
    (6) remeber best.k will take a while to run.

--- topic-model-tester.R
+++ topic-model-tester.R
@@ -1,7 +1,8 @@
+# for either: (1) word cloud or (2) lda/dtm methods that are unsupported with JSON remove from tester
+
 source("topic-model-api.R")
 
 # this tests everything once we have a doc-term-matrix
-
 data("AssociatedPress")
 my.desired.number.of.topics = 2
 my.desired.number.of.top.words.per.topic = 10
@@ -9,48 +10,56 @@ my.desired.number.of.top.words.per.topic = 10
 ap.tidy.text = get.tidy.text.from.dtm(AssociatedPress)
 ap.tidy.text.json = get.tidy.text.from.dtm(AssociatedPress, T)
 # we can make a word cloud of all the text
+validate(ap.tidy.text.json) # use validate to test if it actually is json
 
 ap.word.cloud = get.word.cloud.from.tidy.text(ap.tidy.text)
-ap.word.cloud = get.word.cloud.from.tidy.text(ap.tidy.text, T)
-ap.word.cloud
+ap.word.cloud = get.word.cloud.from.tidy.text(ap.tidy.text, T) # this still returns a ggplot object; it should return the name
+# then could test with file.exists(ap.word.cloud)
+# ap.word.cloud don't run takes a while
 #ggsave("apwordcloud.pdf")
 
 # there are lots of documents in so to get a faceted by doc word cloud we do the first 5
 
 list.of.documents.we.want = c("1", "2", "3", "4", "5")
-filtered.ap.tidy.text = ap.tidy.text %>% filter(document %in% list.of.documents.we.want)
-filtered.ap.word.cloud.per.document = get.word.cloud.from.tidy.text(filtered.ap.tidy.text, per.document = TRUE, T)
-filtered.ap.word.cloud.per.document
 
-ap.lda = get.lda(AssociatedPress, my.desired.number.of.topics)
-ap.lda.json = get.lda(AssociatedPress, my.desired.number.of.topics, T)
+filtered.ap.tidy.text = ap.tidy.text %>% filter(document %in% list.of.documents.we.want)
+filtered.ap.word.cloud.per.document = get.word.cloud.from.tidy.text(filtered.ap.tidy.text, per.document = TRUE, T) # this still returns a ggplot object; it should return the name
+# then could test with file.exists(filtered.ap.word.cloud.per.document)
+# filtered.ap.word.cloud.per.document don't run takes a while
+
+ap.lda = get.lda(AssociatedPress, my.desired.number.of.topics) 
+ap.lda.json = get.lda(AssociatedPress, my.desired.number.of.topics, T) # so ldas are weird so don't JSON wrapper, remove this from tester
 
 ap.topics = get.tidy.topics.from.lda(ap.lda)
-ap.topics.json = get.tidy.topics.from.lda(ap.lda, T)
+ap.topics.json = get.tidy.topics.from.lda(ap.lda, output.json.topics=T) # we need to make sure we provide a binding for everything to be safe.
+validate(ap.topics.json)
+
 ap.top.terms = get.top.terms.from.topics(ap.topics, my.desired.number.of.top.words.per.topic)
 ap.top.terms.json = get.top.terms.from.topics(ap.topics, my.desired.number.of.top.words.per.topic, T)
-ap.plot.of.top.terms = get.plot.for.top.terms(ap.top.terms, T)
-ap.plot.of.top.terms
-###
-###
-###
+validate(ap.top.terms.json)
+
+ap.plot.of.top.terms = get.plot.for.top.terms(ap.top.terms, T) # successfully puts it in my directory
+ap.plot.of.top.terms # THIS ONE IS NULL; it should give me the file path of where it saved
+# return in JSON format then validate it.
+
 
 # ap.topics.per.doc
 
 ap.topics.per.doc = get.tidy.topics.from.lda(ap.lda, per.document = TRUE)
-ap.topics.per.doc.json = get.tidy.topics.from.lda(ap.lda, per.document = TRUE, T)
+ap.topics.per.doc.json = get.tidy.topics.from.lda(ap.lda, per.document = TRUE, T) # let's label this last arguement to be specific; let's be consistent
+validate(ap.topics.per.doc.json)
+
 # we can also plot this but there are way to many docs here, so we limit to the first eight
-
-
 filtered.ap.topics.per.doc = ap.topics.per.doc %>% filter(document %in% list.of.documents.we.want)
-filtered.ap.plot.of.topic.per.doc = get.plot.for.per.document(filtered.ap.topics.per.doc, T)
-filtered.ap.plot.of.topic.per.doc
+filtered.ap.plot.of.topic.per.doc = get.plot.for.per.document(filtered.ap.topics.per.doc, T) # successfully puts it in my directory
+filtered.ap.plot.of.topic.per.doc # THIS ONE IS NULL; it should give me the file path of where it saved
+# return in JSON format then validate it.
 
 # or just use the top document classification and make it a tbl
-
 ap.tidy.docs.classified.into.topics = get.tidy.document.classification.from.lda(ap.topics.per.doc)
 ap.tidy.docs.classified.into.topics.json = get.tidy.document.classification.from.lda(ap.topics.per.doc, T)
-ap.tidy.docs.classified.into.topics
+validate(ap.tidy.docs.classified.into.topics.json)
+
 
 # try and create a doc term matrix from a list of pdf and html docs
 
@@ -58,51 +67,55 @@ my.list.of.docs = c("global_research/156390893402424.pdf",
                     "global_research/156398257833616.pdf",
                     "global_research/156501860173228.html")
 
-list.of.possible.ks = c(3, 4, 5, 6, 7)
+list.of.possible.ks = c(3, 4, 5, 6, 7) # this is used for best.k
+
 my.docs.df = from.a.list.of.files.to.file.text.df(my.list.of.docs)
-my.docs.df.json = from.a.list.of.files.to.file.text.df(my.list.of.docs)
+my.docs.df.json = from.a.list.of.files.to.file.text.df(my.list.of.docs) # this needs a TRUE flag and JSON return
+# once finished then validate(my.docs.df.json)
 
 
 
 #NEED HELP HERE
 
-best.k = find.best.k.for.docs(my.docs.df, list.of.possible.ks)
+best.k = find.best.k.for.docs(my.docs.df, list.of.possible.ks)  # this needs a TRUE flag and JSON return; it can take a while (5 mins)
+# once finished implementation validate(best.k.json)
 
-my.docs.as.tidy.txt = from.file.text.df.to.tidytext(my.docs.df)
+my.docs.as.tidy.txt = from.file.text.df.to.tidytext(my.docs.df) # this needs a TRUE flag and JSON return; be careful lots of default params
+# once finished then validate(my.docs.as.tidy.txt)
 
 # we can make a word cloud of all the text
 
-my.word.cloud = get.word.cloud.from.tidy.text(my.docs.as.tidy.txt, T)
+my.word.cloud = get.word.cloud.from.tidy.text(my.docs.as.tidy.txt, T) # return path, then remove from tester b/c its a word cloud; be careful b/c multiple arguements
 my.word.cloud
 
 # and we can make a word cloud per document
 
-my.word.cloud.per.doc = get.word.cloud.from.tidy.text(my.docs.as.tidy.txt, per.document = TRUE, T)
+my.word.cloud.per.doc = get.word.cloud.from.tidy.text(my.docs.as.tidy.txt, per.document = TRUE, T) # return path, then remove from tester b/c its a word cloud
 my.word.cloud.per.doc
 
 # this gets our very own doc term matrix
 
-my.dtm = from.tidy.text.to.dtm(my.docs.as.tidy.txt)
-
-
-
-
+my.dtm = from.tidy.text.to.dtm(my.docs.as.tidy.txt) # no need to worry about this one b/c dtm cannot go to JSON
 
 # now we can topic model with it
-
 my.lda = get.lda(my.dtm, best.k)
-my.lda.json = get.lda(my.dtm, best.k, T)
+my.lda.json = get.lda(my.dtm, best.k, T) # no need to worry about this one b/c lda cannot go to JSON; can be removed from tester
 
 my.topics = get.tidy.topics.from.lda(my.lda)
-my.topics.json = get.tidy.topics.from.lda(my.lda, T)
+my.topics.json = get.tidy.topics.from.lda(my.lda,  output.json.topics=T) # multiple arguements besure to specify all
+validate(my.topics.json)
 
 my.top.terms = get.top.terms.from.topics(my.topics, my.desired.number.of.top.words.per.topic)
 my.top.terms.json = get.top.terms.from.topics(my.topics, my.desired.number.of.top.words.per.topic, T)
+validate(my.top.terms.json)
 
 #my.top.terms.plot
 
-my.plot.of.top.terms = get.plot.for.top.terms(top.terms, T)
-my.plot.of.top.terms
+my.plot.of.top.terms = get.plot.for.top.terms(my.top.terms) 
+my.plot.of.top.terms = get.plot.for.top.terms(my.top.terms, T) # successfully puts it in my directory
+my.plot.of.top.terms # THIS ONE IS NULL; it should give me the file path of where it saved
+# return in JSON format then validate it.
+
 
 # my.topics.per.doc
 my.topics.per.doc = get.tidy.topics.from.lda(my.lda, per.document = TRUE)
@@ -111,13 +124,15 @@ my.topics.per.doc.json = get.tidy.topics.from.lda(my.lda, per.document = TRUE, T
 # we can also plot this
 my.plot.of.topic.per.doc = get.plot.for.per.document(my.topics.per.doc)
 my.plot.of.topic.per.doc = get.plot.for.per.document(my.topics.per.doc, T)
-my.plot.of.topic.per.doc
+# successfully puts it in my directory
+my.plot.of.topic.per.doc # THIS ONE IS NULL; it should give me the file path of where it saved
+# return in JSON format then validate it.
 
 # or just use the top document classification and make it a tbl
 my.tidy.docs.classified.into.topics = get.tidy.document.classification.from.lda(my.topics.per.doc)
 my.tidy.docs.classified.into.topics.json = get.tidy.document.classification.from.lda(my.topics.per.doc, T)
 my.tidy.docs.classified.into.topics
-
+validate(my.tidy.docs.classified.into.topics.json)
 
 # now take everything in global research and determine the best k for a topic model.
 file.list = list.files("global_research/") 
@@ -125,8 +140,8 @@ file.list = list.files("global_research/")
 # try and create a doc term matrix from a list of pdf and html docs
 all.docs.in.a.directory = paste0("global_research/", file.list)
 
-all.docs.in.a.directory.df = from.a.list.of.files.to.file.text.df(all.docs.in.a.directory)
-all.docs.in.a.directory.df.json = from.a.list.of.files.to.file.text.df(all.docs.in.a.directory, T)
+all.docs.in.a.directory.df = from.a.list.of.files.to.file.text.df(all.docs.in.a.directory) # takes a little bit to run
+all.docs.in.a.directory.df.json = from.a.list.of.files.to.file.text.df(all.docs.in.a.directory, T) # errors out; needs the JSON arguement
 
-best.k = find.best.k.for.docs(all.docs.in.a.directory.df, list.of.possible.ks)
-
+best.k = find.best.k.for.docs(all.docs.in.a.directory.df, list.of.possible.ks) # needs the JSON arguement, takes a very long time to run; allow 30 minutes
+# once done validate best.k
